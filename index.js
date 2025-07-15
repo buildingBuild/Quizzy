b6af87f1f79908fc1e71dc13c56a8a663b5898f1
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2025-07-15 17:12:47 -0500

    Decent Base

---
+++ index.js
@@ -0,0 +1,21 @@
+class Library {
+
+
+
+}
+
+
+const set = []; // will store array of flash_cards
+
+const array_of_flashCards = []; // will store objects of flash cards
+
+const flashCard = {
+    question: "Enter a question",
+    answer: "I am an answer",
+    difficulty: "I range from 1-5 in terms of difficulty",
+};
+
+function pageCreate() {
+    window.location.href = "create.html"
+
+}
